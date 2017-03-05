8d49c17d25870360ee79e54fccf2f871cd387d08
HigashiNana <ssds.dr.hshow@gmail.com>
2017-03-05 17:28:01 +0900

    socket_1

--- js/mongo.js
+++ js/mongo.js
@@ -6,21 +6,21 @@ MongoClient.connect('mongodb://localhost:27017/day5',function(err,db){
   var collection = db.collection('user');
 
   //collection.find().forEach();
-
+/*
   var value = {user_id:"test",status:"logout"};
   //value={user_id:"お昼にしよう"};
   collection.insertOne(value,function(err,r){
     console.log(value);
   });
-/*  collection.updateOne({user_id:"test"},{$set:{user_id:"TEST"}},
+  collection.updateOne({user_id:"test"},{$set:{user_id:"TEST"}},
     function(err,r){
       console.log("修正作業");
     });
   collection.deleteMany({user_id:null},function(err,r){
     console.log("削除");
   });*/
-  db.on('close',function(){
+  /*db.on('close',function(){
     console.log('close DB');
   });
-  db.close();
+  db.close();*/
 });
